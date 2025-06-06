332283d28d58212b83e92360d506a86b2357a6f2
Joe Clark <weirdlookingjay@live.com>
2025-06-06 14:36:31 -0400

    created signup and signin form components

--- schemas/signInSchema.ts
+++ schemas/signInSchema.ts
@@ -1,9 +1,12 @@
-import { z } from "zod";
+import * as z from "zod";
 
 export const signInSchema = z.object({
-  email: z
+  identifier: z
     .string()
-    .email({ message: "Invalid email format" })
-    .min(1, { message: "Email is required" }),
-  password: z.string().min(1, { message: "Password is required" }),
+    .min(1, { message: "Email or username is required" })
+    .email({ message: "Please enter a valid email address" }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
